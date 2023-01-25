bc55227aac27dcd1dbfa97aae0e83cd37c76fac9
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-01-25 14:30:58 -0800

    print first x elements

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    counter = 0
+
+    for i in range(x):
+        try:
+            print("{:}".format(my_list[i], end='')
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        else:
+            counter += 1
+    print()
+    return (counter)
