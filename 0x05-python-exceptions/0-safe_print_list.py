973ab9076dbb2973d24c149455022607d54882de
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-01-25 15:04:58 -0800

    print any typpe var

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    counter = 0
+    count = 0
+
     for i in range(x):
         try:
-            print("{}".format(my_list[i], end=''))
+            print("{}".format(my_list[i]), end='')
         except:
             break
         else:
-            counter += 1
+            count += 1
+
     print()
-    return (counter)
+    return (count)
