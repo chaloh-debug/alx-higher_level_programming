76af5387f3f51abf4be84a24f6d207895f02082a
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-02-07 03:05:30 +0300

    inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
 class BaseGeometry:
     """ Empty class """
     def area(self):
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a base geometry class BaseGeometry."""
+
+
 class BaseGeometry:
     """ Class that defines the attributes of Geometric Shapes """
 
