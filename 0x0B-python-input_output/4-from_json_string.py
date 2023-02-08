ea13eef76b03bb09b0df8a92931c3cdc11d223d2
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-02-09 01:21:32 +0300

    file manipulation with python

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+module to decode a JSON file
+"""
+
+
+def from_json_string(my_str):
+    """
+
+    function that returns an object represented by a JSON string:
+
+    Args:
+        my_str: JSON file
+
+    """
+
+    return json.loads(my_str)
