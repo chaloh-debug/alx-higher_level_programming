cf8b2cc5981dd9bbf194957840899cda1ef65526
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-12 02:46:22 +0300

    first commit

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+if (process.args.length < 3) {
+    console.log("No argument");
+} else if (process.args.length === 3) {
+    console.log("Argument found");
+}
+else {
+    console.log("Arguments found");
+}
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+if (process.argv[2] !== defined) {
+    console.log("No argument");
+}
+else {
+    console.log(process.argv[2]);
+}
