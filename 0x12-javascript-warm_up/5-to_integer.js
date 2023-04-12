8769bdf8147418a2123dc8a65c4d174b948341a7
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-12 05:42:42 +0300

    first commit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const arg = parseInt(process.argv[2]);
-if (Number.isNaN(number)) {
+if (Number.isNaN(arg)) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + arg);
