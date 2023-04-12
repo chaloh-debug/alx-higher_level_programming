f3d76ce7103dee49f4ed44a89abc136585875e77
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-13 00:23:39 +0300

    fisrt commit

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./rectangle.js') {
-	constructor(size) {
-		super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 };
