b1cdf15adfbaa97efa32a38d5c2291a03982bdb7
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-15 12:03:40 +0300

    filtered cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -8,8 +8,8 @@ import sys
 import MySQLdb
 
 def state2cities():
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], db=sys.argv[2],
+                         passwd=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT cities.name \
                  FROM cities INNER JOIN states \
