1749482d2a0e13affaac92fd4c8acd17abd25d67
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-15 08:11:27 +0300

    execute

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-
 """
-lists all states with a name starting with N
+lists all states from the database hbtn_0e_0_usa
 """
 
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port='3306')
-    cur=db.cursor()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
-
     for row in rows:
-        if row[1][0] == "N":
-            print(row)
-
+        print(row)
     cur.close()
     db.close()
