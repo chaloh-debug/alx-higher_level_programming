417bb7dbe18404629cfb14b0a5bc93e22f121237
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-15 22:48:21 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,5 +10,6 @@ Base = declarative_base()
 class State(Base):
     """ state attributes"""
     __tablename__ = 'states'
-    id = Column(Integer, autoincrement=True, unique=True,nullable=False,primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
