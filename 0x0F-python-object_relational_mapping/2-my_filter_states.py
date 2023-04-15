59c3c1a5e845cbb7dce636d02eb97eaf5ca283b4
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-15 07:43:31 +0300

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=hbtn_0e_0_usa, name=sys.argv[4], port=3306)
+    cur = db.cursor()
+    cur.execute(SELECT * FROM states WHERE name )
+
