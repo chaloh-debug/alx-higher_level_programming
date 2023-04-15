b862b7e69737fc5a0cae919d24f578e70e957b80
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-15 08:59:11 +0300

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
     cur = db.cursor()
     cur.execute("SELECT * \
             FROM states \
-            WHERE BINARY name = %s".format(sys.argv[4]))
+            WHERE name = %s".format(sys.argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
