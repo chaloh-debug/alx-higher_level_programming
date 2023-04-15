0e1d52a064378c39c7be3c6e3a99db3f850d6777
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-15 08:35:15 +0300

    pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
+Lists all values in the states tables of a database where name
+matches the argument
 """
 
 import sys
 import MySQLdb
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
     cur = db.cursor()
     cur.execute("SELECT * \
             FROM states \
-            WHERE name LIKE '{}'".format(sys.argv[4]))
+            WHERE BINARY name LIKE '{}'".format(sys.argv[4]))
     rows = cur.fetchall()
 
     for row in rows:
