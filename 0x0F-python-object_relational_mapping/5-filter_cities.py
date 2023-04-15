0b9f6545b151b69a9a2b873c834b3ab2b105ca04
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-04-15 12:22:49 +0300

    filtered cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,11 +12,9 @@ def statecities():
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     cur = db.cursor()
-    cur.execute("SELECT cities.name \
-                 FROM cities INNER JOIN states \
-                 ON states.id = cities.state_id \
-                 WHERE states.name = %s \
-                 ORDER BY cities.id ASC;", [sys.argv[4]])
+    cur.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = '{}';".format(sys.argv[4]))
     rows = cur.fetchall()
 
     print(", ".join([row[1] for row in rows]))
