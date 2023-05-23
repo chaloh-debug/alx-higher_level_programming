db9b5640f1d11f092fbfdba2940d95f3ac6780fd
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-23 22:34:28 +0300

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 const id = process.argv[2];
-const url = ('https://swapi-api.alx-tools.com/api/films/' + id + '/');
 const request = require('request');
-
-request(url, function(error, body, response) {
-    if (error) {
-      console.log(error);
+request('https://swapi-api.alx-tools.com/api/films/' + id + '/', function(error, response, body) {
+    if (error == null) {
+        const data = JSON.parse(body);
+        console.log(data.title);
     }
-    const data = JSON.parse(body);
-    console.log(data.title);
 })
