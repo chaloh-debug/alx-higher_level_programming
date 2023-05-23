76c8241aaf917a00935ceda822f7e392a60b8c27
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-23 22:44:56 +0300

    fix

--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  if (error == null) {
-    console.log('code:' + response.statusCode);
-  }
-});
