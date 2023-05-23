efcd737f67ff6afca80437aa909ca06c5dc7f13a
chaloh-debug <charlesgitahi.cn@gmail.com>
2023-05-23 17:48:24 +0300

    update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,5 +5,5 @@ const request = require('request');
 
 request(url, function(err, response) {
     if (err) throw err;
-    console.log('$(response.statusCode)');
+    console.log('code: ${response.statusCode}');
 })
